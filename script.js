a57a2fc64f9940a60f0e432aa6b9b0a1db224906
Rishikesh Mane <139518215+RishiMane03@users.noreply.github.com>
2023-09-28 16:17:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function updateDateTime() {
+            var timerElement = document.getElementById("timer");
+            var currentDate = new Date();
+            var formattedDate = currentDate.toLocaleDateString();
+            var formattedTime = currentDate.toLocaleTimeString();
+
+            // Display the formatted date and time
+            timerElement.textContent = formattedDate + ", " + formattedTime;
+        }
+
+        // Update the date and time every second
+        setInterval(updateDateTime, 1000);
+
+        // Initial call to display the date and time
+        updateDateTime();
